3c714e97b5f57ddfedb6482888c5e17427479fce
User <User@DESKTOP-3G79H5S>
2023-10-20 09:55:12 +0700

    Добавьте файлы проекта.

---
+++ Test Menu/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Test_Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Header = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PlayGame = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ExitGame = new System.Windows.Forms.Button();
+            this.Menu = new System.Windows.Forms.Panel();
+            this.Header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Header
+            // 
+            this.Header.BackColor = System.Drawing.Color.DimGray;
+            this.Header.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Header.Controls.Add(this.ExitGame);
+            this.Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Header.Location = new System.Drawing.Point(0, 0);
+            this.Header.Margin = new System.Windows.Forms.Padding(4);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(1280, 34);
+            this.Header.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSalmon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(51, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Настройки";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // PlayGame
+            // 
+            this.PlayGame.BackColor = System.Drawing.Color.DarkSalmon;
+            this.PlayGame.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.PlayGame.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSalmon;
+            this.PlayGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PlayGame.Font = new System.Drawing.Font("Shadow Beamer", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayGame.Location = new System.Drawing.Point(16, 13);
+            this.PlayGame.Name = "PlayGame";
+            this.PlayGame.Size = new System.Drawing.Size(202, 59);
+            this.PlayGame.TabIndex = 0;
+            this.PlayGame.Text = "Play";
+            this.PlayGame.UseVisualStyleBackColor = false;
+            this.PlayGame.Click += new System.EventHandler(this.PlayGame_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ExitGame
+            // 
+            this.ExitGame.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ExitGame.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ExitGame.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ExitGame.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSalmon;
+            this.ExitGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitGame.Font = new System.Drawing.Font("Shadow Beamer", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitGame.Location = new System.Drawing.Point(1238, 0);
+            this.ExitGame.Name = "ExitGame";
+            this.ExitGame.Size = new System.Drawing.Size(40, 32);
+            this.ExitGame.TabIndex = 2;
+            this.ExitGame.Text = "X";
+            this.ExitGame.UseVisualStyleBackColor = false;
+            this.ExitGame.Click += new System.EventHandler(this.ExitGame_Click);
+            // 
+            // Menu
+            // 
+            this.Menu.Controls.Add(this.PlayGame);
+            this.Menu.Controls.Add(this.button1);
+            this.Menu.Location = new System.Drawing.Point(44, 421);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(241, 249);
+            this.Menu.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.Header);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("MV Boli", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TestMenu";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Header.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Header;
+        private System.Windows.Forms.Button PlayGame;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ExitGame;
+        private System.Windows.Forms.Panel Menu;
+    }
+}
+
---
+++ Test Menu/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+            Header.Location = new Point(0, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //newButton.MouseMove += NewButton_MouseMove;
+        }
+        //Кнопка убегает
+        //private void NewButton_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    Random rnd = new Random();
+        //    newButton.Location = new Point(rnd.Next(0, 200), rnd.Next(0, 500));
+        //}
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+           this.Controls.Remove(Header);
+            this.Controls.Remove(newButton);
+            //((Button)sender) 
+        }
+        Button newButton = new Button();
+
+        private void PlayGame_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(Menu);
+            this.Controls.Remove(PlayGame);
+        }
+
+        private void ExitGame_Click(object sender, EventArgs e)
+        {   
+            this.Close();
+        }
+    }
+}
